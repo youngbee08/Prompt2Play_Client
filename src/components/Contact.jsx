4fd0aed59e906c49957dd9961a6d978ba05ea267
youngbee08 <youngbeeh08@gmail.com>
2025-07-10 11:21:43 +0100

    updated the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Contact.css'
 import { MessageCircle, Send, User } from 'lucide-react'
+import { toast } from 'sonner'
 
 const Contact = () => {
   return (
     <>
-      <form className="contact" id="contact">
+      <form className="contact" id="contact" onSubmit={(e)=>{e.preventDefault(), toast.success("Your message has been sent successfully, we will get back to you soon")}}>
         <h2>Get in touch</h2>
         <div className="contactInputs">
           <label htmlFor="name"><User size={35}/></label>
           <input required title='First Name' type="text" name='name' placeholder='First Name'/>
-          <input required title='Last Name' type="text" name='name' placeholder='Last Name'/>
+          <input required title='Last Name' type="text" name='name' placeholder='Last Name' className='last'/>
         </div>
         <div className="contactTextarea">
           <label htmlFor="message"><MessageCircle size={35}/></label>
